24e4ac1b469e269c56dfdfdb401470e8607a7f80
Alessandro Paris <par_214@hotmail.com>
2022-05-04 12:13:01 -0500

    Added Cucumber Config

---
+++ src/test/java/com/evoteam/eatsave/cucumber/CucumberConfig.java
@@ -0,0 +1,22 @@
+package com.evoteam.eatsave.cucumber;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.java.DefaultDataTableCellTransformer;
+import io.cucumber.java.DefaultDataTableEntryTransformer;
+import io.cucumber.java.DefaultParameterTransformer;
+
+import java.lang.reflect.Type;
+
+public class CucumberConfig {
+    private final ObjectMapper objectMapper;
+    public CucumberConfig() {
+        objectMapper = new ObjectMapper();
+    }
+    @DefaultDataTableCellTransformer
+    @DefaultDataTableEntryTransformer
+    @DefaultParameterTransformer
+    public Object transform(final Object from, final Type to) {
+        return objectMapper.convertValue(from, objectMapper.constructType(to));
+    }
+}
---
+++ src/test/java/com/evoteam/eatsave/cucumber/CucumberIntegrationTests.java
@@ -0,0 +1,15 @@
+package com.evoteam.eatsave.cucumber;
+
+import com.evoteam.eatsave.EatSaveApplication;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(Cucumber.class)
+@CucumberContextConfiguration
+@SpringBootTest(classes = { EatSaveApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@CucumberOptions(plugin = {"pretty"}, tags = "", features = "src/test/resources/features")
+public class CucumberIntegrationTests {
+}
