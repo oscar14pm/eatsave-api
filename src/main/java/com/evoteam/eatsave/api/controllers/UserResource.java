971af7e87d8695d297e12cdeef489e24619daac8
Alessandro Chumpitaz <par_214@hotmail.com>
2022-06-27 16:48:15 -0500

    chore: added cors

--- src/main/java/com/evoteam/eatsave/api/controllers/UserResource.java
+++ src/main/java/com/evoteam/eatsave/api/controllers/UserResource.java
@@ -69,7 +69,7 @@ public class UserResource {
                 User user = userService.getUser(username);
                 String access_token = JWT.create()
                         .withSubject(user.getUsername())
-                        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1001000))
                         .withIssuer(request.getRequestURL().toString())
                         .withClaim("roles", user.getRoles().stream().map(Role::getName).collect(Collectors.toList()))
                         .sign(algorithm);
--- src/main/java/com/evoteam/eatsave/security/SecurityConfig.java
+++ src/main/java/com/evoteam/eatsave/security/SecurityConfig.java
@@ -13,6 +13,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -34,12 +40,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter filter = new CustomAuthenticationFilter(authenticationManagerBean());
         filter.setFilterProcessesUrl("/api/login");
+        http.cors().and().csrf().disable();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests().antMatchers("/**").permitAll();
         http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh/**").permitAll();
         http.authorizeRequests().antMatchers(POST, "/api/clients/**").permitAll(); // Create new client
-        http.authorizeRequests().antMatchers("/swagger-resources/**", "/swagger-ui/**", "/v2/api-docs/**", "/webjars/**").permitAll(); // Swagger Utils
+        http.authorizeRequests().antMatchers(
+                "/swagger-resources/**",
+                "/swagger-ui/**",
+                "/v2/api-docs/**",
+                "/webjars/**").permitAll();
         http.authorizeRequests().antMatchers("/api/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_CLIENT");
         http.authorizeRequests().antMatchers(GET, "/api/consultant/**").hasAnyAuthority("ROLE_ADMIN","ROLE_CONSULTANT");
         http.authorizeRequests().antMatchers(GET, "/api/user/create/**").hasAnyAuthority("ROLE_ADMIN");
@@ -53,5 +64,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*")); //or add * to allow all origins
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); //to set allowed http methods
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        //configuration.setExposedHeaders(Arrays.asList("custom-header1", "custom-header2"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
